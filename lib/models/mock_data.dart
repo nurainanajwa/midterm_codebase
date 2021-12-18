95ea0ba02ae9be940acd4616310e2f0851c99f31
Nuraina Najwa <nurainanajwa@graduate.utm.my>
2021-12-18 14:49:58 +0800

    commit

--- lib/models/mock_data.dart
+++ lib/models/mock_data.dart
@@ -1,11 +1,33 @@
 //! Create the mock database here which consists of a list of notes. The note data is hard coded.
 
+//import 'package:flutter/cupertino.dart';
+
 import 'note.dart';
 
-final mockNoteList = <Note>[];
+//final mockNoteList = <Note>[];
+
+
+List<Note> mockNoteList = [
+  Note(
+    title: "What is Flutter?",
+    content: "Flutter is an open-source UI framework for creating native mobile applications. It allows developers to build mobile applications a single codebase."
+  ),
+
+  Note(
+    title: "Stateful Widgets",
+    content: "The widgets whose state can be altered once they are built are called stateful Widgets. ",
+  ),
+
+  Note(
+    title: "Conditional (ternary) operator",
+    content: "The conditional (ternary) operator in Dart takes three operands: a condition, first expression (if truthy) and second expression (if falsy).",
+  ),
+  
+];
 
 //? Here are some sample data that you can copy-paste in order to create the mock database
 //? --------------------------------------------------------------------------------------
+
 
 /*
 What is Flutter?
--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,3 +1,22 @@
 //! Define the model class here
 
-class Note {}
+import 'package:flutter/cupertino.dart';
+
+class Note {
+  String _title;
+  String _content;
+  bool _selected;
+
+  String get title => _title;
+  String get content => _content;
+  bool get selected => _selected;
+
+  Note({title, content, selected = false})
+  : _title = title,
+    _content = content,
+    _selected = selected;
+  Note.copy(Note from): this(title: from.title, content: from.content, selected: from.selected);
+
+
+
+}
